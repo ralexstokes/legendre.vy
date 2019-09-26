c66897dc8ec938e5bb374debd789bd1bfb869e98
Dankrad Feist <mail@dankradfeist.de>
2019-09-26 22:32:00 +0100

    Variants for more efficient Legendre computation and multi-test

--- contract/legendre_bit.vy
+++ contract/legendre_bit.vy
@@ -1,6 +1,6 @@
 LOOP_ROUNDS: constant(uint256) = 2**32
 
-@public
+@private
 @constant
 def legendre_bit(input_a: uint256, q: uint256) -> uint256:
     a: uint256 = input_a
@@ -42,3 +42,70 @@ def legendre_bit(input_a: uint256, q: uint256) -> uint256:
             return 0
     else:
         return 1
+
+
+@private
+@constant
+def legendre_bit_expmod(input_a: uint256, q: uint256) -> uint256:
+    a: uint256 = input_a
+    if a >= q:
+        a = a % q
+    if a == 0:
+        return 1
+
+    assert(q > a and q % 2 == 1)
+
+    e: uint256 = (q - 1) / 2
+    x: uint256 = a
+    c: uint256 = 1
+
+    for i in range(256):
+        if e == 0:
+            break
+        if e % 2 == 1:
+            c = uint256_mulmod(c, x, q)
+        x = uint256_mulmod(x, x, q)
+        e /= 2
+    
+    if c == q - 1:
+        return 0
+    return 1
+
+
+@private
+def legendre_bit_rawcall_expmod(input_a: uint256, q: uint256) -> uint256:
+    a: uint256 = input_a
+    if a >= q:
+        a = a % q
+    if a == 0:
+        return 1
+
+    assert(q > a and q % 2 == 1)
+
+    e: uint256 = (q - 1) / 2
+    b: uint256 = 32
+
+    c: uint256 = convert(raw_call(0x0000000000000000000000000000000000000005, 
+    concat(convert(b, bytes32), convert(b, bytes32), convert(b, bytes32),
+    convert(a, bytes32),
+    convert(e, bytes32),
+    convert(q, bytes32)
+    ), gas=100000, outsize=32), uint256)
+    
+    if c == q - 1:
+        return 0
+    return 1
+
+@public
+def legendre_bit_multi(input_a: uint256, q: uint256, input_n: uint256) -> uint256:
+    a: uint256 = input_a
+    r: uint256 = 0
+    n: uint256 = input_n
+    for i in range(256):
+        r *= 2
+        r += self.legendre_bit_rawcall_expmod(a, q)
+        a += 1
+        n -= 1
+        if n == 0:
+            break
+    return r
--- tests/test_legendre.py
+++ tests/test_legendre.py
@@ -16,15 +16,23 @@ PRIMES = [
 1208925819614629174706189,
 1267650600228229401496703205653,
 356811923176489970264571492362373784095686747,
-57896044618658097711785492504343953926634992332820282019728792003956564820063
+#57896044618658097711785492504343953926634992332820282019728792003956564820063
 ]
 
 VALUES = [randint(0, 2**256 - 1) for i in range(10)]
 
 def jacobi_bit_mpz(a, n):
-    return 1 if jacobi(a, n) >= 0 else 0 
+    return 1 if jacobi(a, n) >= 0 else 0
+
+def jacobi_bit_multi(a, n, m):
+    r = 0
+    for i in range(m):
+        r *= 2
+        r += jacobi_bit_mpz(a + i, n)
+    return r
 
 
+"""
 @pytest.mark.parametrize(
     'value,prime,expected_result',
     [
@@ -37,5 +45,40 @@ def test_legendre_bit(legendre_bit_contract,
                       prime,
                       expected_result):
     call = legendre_bit_contract.functions.legendre_bit(value, prime)
+    print(call.estimateGas())
     result = call.call()
     assert expected_result == result
+"""
+
+"""
+@pytest.mark.parametrize(
+    'value,prime,expected_result',
+    [
+        (v % p, p, jacobi_bit_mpz(v % p, p)) for p in PRIMES for v in VALUES
+    ]
+)
+def test_legendre_bit_expmod(legendre_bit_contract,
+                      w3,
+                      value,
+                      prime,
+                      expected_result):
+    call = legendre_bit_contract.functions.legendre_bit_expmod(value, prime)
+    print(call.estimateGas())
+    result = call.call()
+    assert expected_result == result
+"""
+
+@pytest.mark.parametrize(
+    'value,prime',
+    [
+        (v, p) for p in PRIMES for v in VALUES
+    ]
+)
+def test_legendre_bit_multi(legendre_bit_contract,
+                      w3,
+                      value,
+                      prime):
+    call = legendre_bit_contract.functions.legendre_bit_multi(value, prime, 100)
+    print(call.estimateGas())
+    result = call.call()
+    assert result == jacobi_bit_multi(value, prime, 100)
